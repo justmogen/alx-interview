dcba9a0fe01b7d72b2b15ad0460a784c855c14aa
justmogen <justmogen@gmail.com>
2023-09-29 00:38:40 +0300

    using zip to create a pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,12 @@
 def pascal_triangle(n):
+
+
     triangle = []
     for p in range(n):
         row = [1]
         if triangle:
             last_rows = triangle[-1]
             row.extend([sum(pair) for pair in zip(last_rows, last_rows[1:])])
-        row.append(1)
+            row.append(1)
         triangle.append(row)
-
     return triangle

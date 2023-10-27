8b11318424e5900349007ae45080ce88ae8f66f2
justmogen <justmogen@gmail.com>
2023-10-27 16:19:48 +0300

    simplified code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,22 +1,28 @@
-#!/usr/bin/python3
+# /usr/bin/python3
 """UTF-8 Validation"""
 
 
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding."""
-    bytes = 0
+    num_bytes_to_follow = 0
+
     for num in data:
-        mask = 1 << 7
-        if not bytes:
-            while mask & num:
-                bytes += 1
-                mask >>= 1
-            if not bytes:
-                continue
-            if bytes == 1 or bytes > 4:
+        # Check if it's a continuation byte
+        if num_bytes_to_follow > 0:
+            if (num >> 6) != 0b10:
                 return False
+            num_bytes_to_follow -= 1
         else:
-            if num >> 6 != 0b10:
+            # Determine the number of bytes for the current character
+            if (num >> 7) == 0b0:
+                num_bytes_to_follow = 0
+            elif (num >> 5) == 0b110:
+                num_bytes_to_follow = 1
+            elif (num >> 4) == 0b1110:
+                num_bytes_to_follow = 2
+            elif (num >> 3) == 0b11110:
+                num_bytes_to_follow = 3
+            else:
                 return False
-        bytes -= 1
-    return bytes == 0
+
+    # All characters have correct continuation bytes
+    return num_bytes_to_follow == 0

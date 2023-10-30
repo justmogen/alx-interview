4da8a868a73cb1a0f29d62d985cb0cfc2c6c062f
justmogen <justmogen@gmail.com>
2023-10-30 14:13:11 +0300

    nqueens interview puzzle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def is_safe(board, row, col):
     # Check if there is a queen in the same column
     for i in range(row):
@@ -10,25 +11,28 @@ def is_safe(board, row, col):
             return False
     return True
 
+
 def solve_nqueens_util(n, row, board, solutions):
     if row == n:
         solutions.append([[i, board[i]] for i in range(n)])
         return
-    
+
     for col in range(n):
         if is_safe(board, row, col):
             board[row] = col
             solve_nqueens_util(n, row + 1, board, solutions)
 
+
 def solve_nqueens(n):
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     board = [-1] * n
     solutions = []
     solve_nqueens_util(n, 0, board, solutions)
     return solutions
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -39,7 +43,7 @@ if __name__ == "__main__":
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
@@ -47,4 +51,3 @@ if __name__ == "__main__":
     solutions = solve_nqueens(N)
     for solution in solutions:
         print(solution)
-

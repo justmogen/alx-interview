d88b2bb3f7c9f15118111eb81edae0a4e17b105e
justmogen <justmogen@gmail.com>
2023-11-10 22:55:04 +0300

    starwars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,16 +3,16 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 const movieId = process.argv[2];
 request(url + movieId, function (error, response, body) {
-    if (error) {
+  if (error) {
+    console.error(error);
+  }
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    request(character, function (error, response, body) {
+      if (error) {
         console.error(error);
-    }
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-        request(character, function (error, response, body) {
-        if (error) {
-            console.error(error);
-        }
-        console.log(JSON.parse(body).name);
-        });
-    }
+      }
+      console.log(JSON.parse(body).name);
     });
+  }
+});
